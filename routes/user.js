f62c046fd958901523b6c9b5ecb0ea49e033490e
wanatpong <bas10664@gmail.com>
2019-05-02 11:50:12 +0700

    Countuserall_wanatpong

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 
+
 router.get("/", (req, res) => {
     res.send({
         status: "user"
